a90e473f5ca38b1b37145112b249a773030446c6
wkohlenberg <walterkohlenberg@gmail.com>
2019-06-10 10:41:46 +0200

    Fix: read result from register before setting start bit to low.

--- software/ffnn_project/main.c
+++ software/ffnn_project/main.c
@@ -8,7 +8,9 @@ int main()
 
   // initialize
   // Send start signal
-  //IOWR_ALTERA_AVALON_PIO_DATA(START_NEURON_1_BASE, 0);
+  IOWR_ALTERA_AVALON_PIO_DATA(START_NEURON_1_BASE, 0);
+
+  printf("NODE 1:\n\n");
 
   int input_1 = 255;
   int input_2 = 0;
@@ -42,8 +44,8 @@ int main()
   printf("Ready flag before start signal: %d\n", IORD_ALTERA_AVALON_PIO_DATA(READY_NEURON_1_BASE));		// this should be zero
 
   // Send start signal
-  printf("Send start signal...");
-  for (int i = 0; i<1000000;i++){}
+  printf("Send start signal...\n");
+  for (int i = 0; i<200000;i++){}
   IOWR_ALTERA_AVALON_PIO_DATA(START_NEURON_1_BASE, 1);
 
   // Wait for ready signal
@@ -53,16 +55,63 @@ int main()
   }while(ready != 1);
 
   // Send start signal - set back to idle
-  //IOWR_ALTERA_AVALON_PIO_DATA(START_NEURON_1_BASE, 0);
-  //printf("Set start signal at 0");
+  int result = IORD_ALTERA_AVALON_PIO_DATA(OUTPUT_NEURON_1_BASE);
+  IOWR_ALTERA_AVALON_PIO_DATA(START_NEURON_1_BASE, 0);
+  printf("Set start signal at 0, so the state is idle from now on.\n");
 
   // read result from neuron
-  int result = IORD_ALTERA_AVALON_PIO_DATA(OUTPUT_NEURON_1_BASE);
   printf("result: %d\n", result);
   double dResult = (double)result / 256.0;
 
   printf("Output of neuron is %f\n", dResult);
 
+  printf("NODE 2: \n\n");
+  for (int i = 0; i<200000;i++){}
+  input_1 = 255;
+  input_2 = 255;
+  printf("input 1: %d\n", input_1);
+  printf("input 2: %d\n", input_2);
+  dWeight_1 = 1.21412 * 256.0;
+  dWeight_2 = 1.32035 * 256.0;
+
+  iWeight_1 = (int)dWeight_1;
+  iWeight_2 = (int)dWeight_2;
+  printf("weight 1: %d\n", iWeight_1);
+  printf("weight 2: %d\n", iWeight_2);
+
+  // Send input data like inputs, weights etc.
+  IOWR_ALTERA_AVALON_PIO_DATA(INPUT_1_NEURON_1_BASE, input_1);
+  IOWR_ALTERA_AVALON_PIO_DATA(INPUT_2_NEURON_1_BASE, input_2);
+  IOWR_ALTERA_AVALON_PIO_DATA(WEIGHT_1_NEURON_1_BASE, iWeight_1);
+  IOWR_ALTERA_AVALON_PIO_DATA(WEIGHT_2_NEURON_1_BASE, iWeight_2);
+
+  // Send number of inputs
+  nInputs = 2-1;				// counting starts with zero
+  IOWR_ALTERA_AVALON_PIO_DATA(NINPUTS_NEURON_1_BASE, nInputs);
+
+  printf("Ready flag before start signal: %d\n", IORD_ALTERA_AVALON_PIO_DATA(READY_NEURON_1_BASE));		// this should be zero
+
+  // Send start signal
+  printf("Send start signal...\n");
+  for (int i = 0; i<200000;i++){}
+  IOWR_ALTERA_AVALON_PIO_DATA(START_NEURON_1_BASE, 1);
+
+  // Wait for ready signal
+  ready = 0;
+  do{
+	ready = IORD_ALTERA_AVALON_PIO_DATA(READY_NEURON_1_BASE);
+  }while(ready != 1);
+
+  // Send start signal - set back to idle
+  result = IORD_ALTERA_AVALON_PIO_DATA(OUTPUT_NEURON_1_BASE);
+  IOWR_ALTERA_AVALON_PIO_DATA(START_NEURON_1_BASE, 0);
+  printf("Set start signal at 0, so the state is idle from now on.\n");
+
+  // read result from neuron
+  printf("result: %d\n", result);
+  dResult = (double)result / 256.0;
+
+  printf("Output of neuron is %f\n", dResult);
 
   return 0;
 }
